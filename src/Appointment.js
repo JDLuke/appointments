6e8ae2d231da0190d73aba10f456a62c63856ded
J.D. Luke <jd@jdluke.com>
2023-02-26 07:57:20 -0500

    AppointmentsDayView tests

--- src/Appointment.js
+++ src/Appointment.js
@@ -1,1 +1,9 @@
+import React from 'react'
 export const Appointment = ({ customer }) => customer.firstName;
+
+export const AppointmentsDayView = ({appointments}) => (
+    <div id="appointmentsDayView">
+        <ol>
+            {appointments.map((appointment) => (<li key={appointment.startsAt} />))}
+        </ol>
+    </div>);
