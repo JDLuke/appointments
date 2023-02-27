0178d5ec1c95733cfdf96aae4682cc50cdbd9624
J.D. Luke <jd@jdluke.com>
2023-02-27 06:04:49 -0500

    Now displaying appointment time

--- src/Appointment.js
+++ src/Appointment.js
@@ -1,9 +1,20 @@
 import React from 'react'
+
+const appointmentTimeOfDay = (startsAt) => {
+    const[h,m] = new Date(startsAt)
+        .toTimeString()
+        .split(":");
+    return `${h}:${m}`;
+}
 export const Appointment = ({ customer }) => customer.firstName;
 
 export const AppointmentsDayView = ({appointments}) => (
     <div id="appointmentsDayView">
         <ol>
-            {appointments.map((appointment) => (<li key={appointment.startsAt} />))}
+            {appointments.map((appointment) => (
+                <li key={appointment.startsAt} >
+                    {appointmentTimeOfDay(appointment.startsAt)}
+                </li>
+            ))};
         </ol>
     </div>);
