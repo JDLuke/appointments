23fe469c76f2c751aa0201d0d3d213762998cb24
J.D. Luke <jd@jdluke.com>
2023-03-01 09:22:54 -0500

    input field

--- src/CustomerForm.js
+++ src/CustomerForm.js
@@ -1,2 +1,4 @@
 import React from "react";
-export const CustomerForm = () => <form />;
+export const CustomerForm = ({original}) => (<form >
+  <input type="text" name="firstName" value={original.firstName} readOnly/>
+  </form>);
--- test/CustomerForm.test.js
+++ test/CustomerForm.test.js
@@ -3,12 +3,28 @@ import {form, initializeReactContainer, render} from "./reactTestExtensions";
 import {CustomerForm} from "../src/CustomerForm";
 
 describe("CustomerForm", () => {
+  const blankCustomer = { firstName: "" }
   beforeEach(() => {
     initializeReactContainer();
   });
 
   it("renders a form", () => {
-    render(<CustomerForm />);
+    render(<CustomerForm original={blankCustomer}/>);
     expect(form()).not.toBeNull();
+  })
+
+  it("renders the first name field as a text box", () => {
+    render(<CustomerForm original={blankCustomer}/>);
+    const field = form().elements.firstName
+    expect(field).not.toBeNull();
+    expect(field.tagName).toEqual("INPUT");
+    expect(field.type).toEqual("text");
+  })
+
+  it("includes the existing first name value", () => {
+    const customer = {firstName: "Ashley"};
+    render(<CustomerForm original={customer} />)
+    const field = form().elements.firstName;
+    expect(field.value).toEqual("Ashley");
   })
 });
