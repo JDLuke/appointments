bd34b5a53e326439f328fcd7f70f6a6c13f87f96
J.D. Luke <jd@jdluke.com>
2023-03-04 07:27:03 -0500

    Matcher tests are pretty clean now, thinking about parameterizing them.

--- test/matchers/toBeInputFieldOfType.test.js
+++ test/matchers/toBeInputFieldOfType.test.js
@@ -5,51 +5,42 @@ import {stripTerminalColor} from "../reactTestExtensions";
 
 describe('toBeInputFieldOfType matcher', () => {
   const displayElement = {type: "text", tagName: "DISPLAY"};
-
-  it("returns pass is true when field is of input type", () => {
-    const domElement = {type: "text", tagName: "INPUT"};
-    const result = toBeInputFieldOfType(domElement, "text");
-    expect(result.pass).toBe(true);
+  const numberElement = {type: "numeric", tagName: "INPUT"};
+  const textInput = {type: "text", tagName: "INPUT"}
+  it("returns pass is true when field is of expected input type", () => {
+    expect(toBeInputFieldOfType(textInput, "text").pass).toBe(true);
   })
 
   it("returns pass is false when type is not text", () => {
-    const domElement = {type: "numeric", tagName: "INPUT"};
-    const result = toBeInputFieldOfType(domElement, "text");
-    expect(result.pass).toBe(false);
+    expect(toBeInputFieldOfType(numberElement, "text").pass).toBe(false);
   })
   it("returns correct message when type is not text", () => {
-    const domElement = {type: "numeric", tagName: "INPUT"};
-    const result = toBeInputFieldOfType(domElement, "text");
-    expect(result.pass).toBe(false);
+    expect(toBeInputFieldOfType(numberElement, "text").pass).toBe(false);
+  })
+  it("returns correct message when type is not text", () => {
+    expect(toBeInputFieldOfType(numberElement, "text").pass).toBe(false);
   })
 
   it("returns pass is false when not an input field", () => {
-    const result = toBeInputFieldOfType(displayElement, "text");
-    expect(result.pass).toBe(false);
+    expect(toBeInputFieldOfType(displayElement, "text").pass).toBe(false);
   });
   it("returns correct message when not an input field", () => {
-    const result = toBeInputFieldOfType(displayElement, "text");
-    expect(stripTerminalColor(result.message())).toContain(`Actual type: "DISPLAY"`)
+    expect(stripTerminalColor(toBeInputFieldOfType(displayElement, "text").message())).toContain(`Actual type: "DISPLAY"`)
   })
 
   it("returns correct message when not an input field", () => {
-    const result = toBeInputFieldOfType(displayElement, "text");
-    expect(stripTerminalColor(result.message())).toContain(`expect(element).toBeInputFieldOfType("text")`)
+    expect(stripTerminalColor(toBeInputFieldOfType(displayElement, "text").message())).toContain(`expect(element).toBeInputFieldOfType("text")`)
   })
 
   it("returns pass is false when null", () => {
-    const result = toBeInputFieldOfType(null, "text");
-    expect(result.pass).toBe(false);
+    expect(toBeInputFieldOfType(null, "text").pass).toBe(false);
   });
 
   it("returns correct message when null", () => {
-    const result = toBeInputFieldOfType(null, "text");
-    expect(stripTerminalColor(result.message())).toContain(`expect(element).toBeInputFieldOfType("text")`)
+    expect(stripTerminalColor(toBeInputFieldOfType(null, "text").message())).toContain(`expect(element).toBeInputFieldOfType("text")`)
   })
 
   it("returns correct actual when null", () => {
-    const result = toBeInputFieldOfType(null, "text");
-    expect(stripTerminalColor(result.message())).toContain(`Actual type: "null"`)
+    expect(stripTerminalColor(toBeInputFieldOfType(null, "text").message())).toContain(`Actual type: "null"`)
   })
-
 })
